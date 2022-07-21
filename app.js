f92fa3b9f1364f981a592fc98c4cc6d946bf977f
GilbertoBarbosa <gisantos555gmail.com>
2022-07-20 22:34:28 -0300

    Voltando a um estado anterior do arquivo

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-// Mais um console log de teste
-console.log("O gisantos10 fez esse commite sem você ver na aula")
-console.log("Enviando para o GitHub")
