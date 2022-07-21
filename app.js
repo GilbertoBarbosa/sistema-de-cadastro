ad219a5cc720775e27fbbbf555af39f6bd2717f3
gisantos10 <30946161+gisantos10@users.noreply.github.com>
2022-07-20 22:02:44 -0300

    Atualizando o arquivo js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// Mais um console log de teste
+console.log("O gisantos10 fez esse commite sem você ver na aula")
